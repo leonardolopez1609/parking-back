e9645c77bd012b9d6374840e6ac213f06a21df4f
leonardolopez1609 <leonardojoselc@ufps.edu.co>
2023-03-07 10:28:26 -0500

    V1.20- Update UserController

--- src/main/java/com/nelumbo/parking/back/controllers/UserController.java
+++ src/main/java/com/nelumbo/parking/back/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.nelumbo.parking.back.entities.User;
 import com.nelumbo.parking.back.services.IUserService;
@@ -31,53 +33,59 @@ public class UserController {
 	private IUserService userService;
 
 	//Revisado
-	@GetMapping("/{id}")
-	public ResponseEntity<?> getUser(@PathVariable Long id) {
+	@GetMapping(path="/{id}",consumes = MediaType.ALL_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(value = HttpStatus.OK)
+	public User getUser(@PathVariable Long id) {
 
-		return new ResponseEntity<User>(userService.findById(id).get(), HttpStatus.OK);
+		return userService.findById(id).get();
 	}
 
 
-	//Agregar al DTO del vehiculo la fecha de ingreso
-	@GetMapping("getparkings/{id}")
-	public ResponseEntity<?> getParkings(@PathVariable Long id) {
+	//Revisado
+	@GetMapping(path="/{id}/parkings",consumes = MediaType.ALL_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(value = HttpStatus.OK)
+	public List<ParkingVehicleDTO> getParkings(@PathVariable Long id) {
 
-		return new ResponseEntity<List<ParkingVehicleDTO>>(userService.findAllParkingsInd(id), HttpStatus.OK);
+		return userService.findAllParkingsInd(id);
 	}
 
 	
 	//Revisado
-	@PostMapping
-	public ResponseEntity<?> createUser(@Valid @RequestBody User user) {
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Map<String, Object> createUser(@Valid @RequestBody User user) {
 
 		Map<String, Object> response = new HashMap<>();
 
 		response.put("user", userService.create(user));
 		response.put("mensaje", "Usuario creado con éxito!");
 
-		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+		return response;
 	}
 	
-	@PostMapping("sendemail")
-	public ResponseEntity<?> sendEmail(@Valid @RequestBody EmailContentDTO emailContent) {
+	//Por implementar
+	@PostMapping(path="/email",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public Map<String, Object> sendEmail(@Valid @RequestBody EmailContentDTO emailContent) {
 
 		Map<String, Object> response = new HashMap<>();
 
 		response.put("mensaje", userService.sendEmail(emailContent));
 
-		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+		return response;
 	}
     
 	
 	//Revisado
-	@PutMapping("/{iduser}/associate/{idparking}")
-	public ResponseEntity<?> asignateParking(@PathVariable Long idparking, @PathVariable Long iduser) {
+	@PutMapping(path="/{iduser}/associate/parkings/{idparking}",consumes = MediaType.ALL_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(value = HttpStatus.OK)
+	public Map<String, Object> asignateParking(@PathVariable Long idparking, @PathVariable Long iduser) {
        
 		userService.associateParking(iduser, idparking);
 		Map<String, Object> response = new HashMap<>();
 		response.put("mensaje", "Parqueadero asociado con éxito!");
 
-		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+		return response;
 
 	}
 }
